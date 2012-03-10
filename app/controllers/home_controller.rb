a5fbe9fd09c95722edf327fa88ec36deda54c0f8
Christian Brel <ch.brel@gmail.com>
2012-03-10 21:07:08 +0100

    Update app/controllers/home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
   	user = User.new
   	
-  	@test = user.encrypt_password('mWindu02')
+  	@test = user.encrypt_password('mdp')
   end
 
 end
